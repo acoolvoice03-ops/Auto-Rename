89c918fd4f92179069b22dee5005de84f895e9fd
acoolvoice03-ops <acoolvoice03@gmail.com>
2025-12-18 00:57:27 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -451,7 +451,7 @@ async def show_start_message(client, message: Message):
         ],
         [
             InlineKeyboardButton('• ᴀʙᴏᴜᴛ', callback_data='about'),
-            InlineKeyboardButton('Dᴇᴠᴇʟᴏᴘᴇʀ•', url='https://t.me/funnytamilan ')
+            InlineKeyboardButton('Dᴇᴠᴇʟᴏᴘᴇʀ•', url='https://t.me/funnytamilan')
         ]
     ])
 
